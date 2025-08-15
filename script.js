42977ec61204482c44afe2dcaf91cb798ddbe8a9
sashank-sahoo <sashanksahoo8@gmail.com>
2025-08-15 16:54:10 +0530

    file changed

---
+++ script.js
@@ -0,0 +1,36 @@
+let calcNumBtn = document.querySelectorAll(".num-btn");
+let acBtn = document.getElementById("ac-btn");
+let output = document.getElementById("calc-output2");
+let equal = document.getElementById("equals-btn");
+let clearBtn = document.getElementById("clear-btn");
+let string = "";
+
+const totalValue = calcNumBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    console.log(e.currentTarget.value);
+    string += e.currentTarget.value;
+    output.innerHTML = string;
+  });
+  return string;
+});
+
+clearBtn.addEventListener("click", () => {
+  string = string.slice(0,-1) || "";
+  output.innerHTML = string || "0";
+})
+
+acBtn.addEventListener("click", (e) => {
+  string = "";
+  output.innerHTML = "0";
+});
+
+equal.addEventListener("click", () => {
+  try {
+    let expression = string.replace(/%/g, "/100*");
+    let result = eval(expression);
+    output.innerHTML = result;
+    string = result.toString();
+  } catch (err) {
+    output.innerHTML = "Error";
+  }
+});
